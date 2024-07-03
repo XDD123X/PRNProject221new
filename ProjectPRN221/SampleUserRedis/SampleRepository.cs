c0c1289e38ff15e9ce3612229a0ca61d6730c56b
hieu.nguyen15 <hieu.nguyen15@sotatek.com>
2024-07-03 21:39:45 +0700

    config redis and sample usage

---
+++ ProjectPRN221/SampleUserRedis/SampleRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectPRN221.SampleUserRedis
+{
+    public class SampleRepository
+    {
+        public async Task<string> GetUsersAsync()
+        {
+            string output = "accesToken";
+
+            await Task.Delay(3000); // simulating data access time
+
+            return output;
+        }
+    }
+}
