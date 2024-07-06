6829b567a7c6621083f4dd11e8546f515fc40dd3
hieu.nguyen15 <hieu.nguyen15@sotatek.com>
2024-07-07 01:07:03 +0700

    develop register and active account feat

--- ProjectPRN221/obj/Debug/net6.0/ProjectPRN221.AssemblyInfo.cs
+++ ProjectPRN221/obj/Debug/net6.0/ProjectPRN221.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPRN221")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b47b33fc4c9e3333aae600824995d35787d2e56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPRN221")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPRN221")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
