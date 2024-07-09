10ab570d45d4634341199258d0939e44e04f665d
Quan <quanhdpt@gmail.com>
2024-07-09 15:02:41 +0700

    course detail

--- ProjectPRN221/Pages/Courses/Details.cshtml.cs
+++ ProjectPRN221/Pages/Courses/Details.cshtml.cs
@@ -12,8 +12,11 @@ namespace ProjectPRN221.Pages.Courses
     public class DetailsModel : PageModel
     {
         private readonly ProjectPRN221.Models.PROJECT_PRUContext _context = new PROJECT_PRUContext();
-
-      public Course Course { get; set; } = default!; 
+        public Course Course { get; set; } = default!;
+        public User Lecture { get; set; } = default!;
+        public IList<Explode> Explode { get; set; } = default!;
+        public int Enrolled { get; set; } = default!;
+        public int Duration { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(long? id)
         {
@@ -25,15 +28,56 @@ namespace ProjectPRN221.Pages.Courses
             var course = await _context.Courses
                 .Include(c => c.Explodes)
                 .FirstOrDefaultAsync(m => m.Id == id);
+            Enrolled = await _context.EnroledCourses.Where(e => e.CourseId == id).CountAsync();
+
+            var explode = await _context.Explodes.Where(e => e.CourseId == id).ToListAsync();
+
+            Duration = await _context.Explodes.Where(e => e.CourseId != id).CountAsync();
+
+            var lecture = await _context.Users
+                            .FirstOrDefaultAsync(l => l.Id == course.UserId);
             if (course == null)
             {
                 return NotFound();
             }
-            else 
-            {
-                Course = course;
-            }
+
+            Course = course;
+            Explode = explode;
+            Lecture = lecture;
+
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(long? id)
+        {
+            if (id == null || _context.Courses == null)
+            {
+                return NotFound();
+            }
+
+            if (User.Identity.IsAuthenticated && User.IsInRole("student"))
+            {
+                var userId = User.Claims.FirstOrDefault(c => c.Type == "UserId")?.Value;
+                if (userId != null)
+                {
+                    var enrollment = new EnroledCourse
+                    {
+                        CourseId = (long)id,
+                        UserId = long.Parse(userId),
+                        CreatedAt = DateTime.Now
+                    };
+
+                    _context.EnroledCourses.Add(enrollment);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            else if (!User.Identity.IsAuthenticated)
+            {
+                //trang dang nhap la gì day v:
+                return RedirectToPage("/Account/Login");
+            }
+
+            return RedirectToPage(new { id = id });
         }
     }
 }
