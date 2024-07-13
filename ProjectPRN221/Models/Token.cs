72b8186e8d09e2723d9038f5cad4060fe23d6e8d
Quan <quanhdpt@gmail.com>
2024-07-13 15:01:45 +0700

    explode detail

--- ProjectPRN221/Models/Token.cs
+++ ProjectPRN221/Models/Token.cs
@@ -10,8 +10,6 @@ namespace ProjectPRN221.Models
         public string? Type { get; set; }
         public DateTime? ExpiredDate { get; set; }
         public bool? IsDeleted { get; set; }
-        public string? Content { get; set; }
-        public string ? Email { get; set; }
 
         public virtual User? User { get; set; }
     }
