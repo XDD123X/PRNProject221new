19fc5406bd1c79d700a6468d908c1af6ea74aa7a
hieu.nguyen15 <hieu.nguyen15@sotatek.com>
2024-07-21 22:55:28 +0700

    dev export excel

--- ProjectPRN221/Pages/Profile.cshtml.cs
+++ ProjectPRN221/Pages/Profile.cshtml.cs
@@ -1,3 +1,4 @@
+using ClosedXML.Excel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Data.SqlClient;
@@ -137,6 +138,121 @@ namespace ProjectPRN221.Pages
             }
 
 			return point;
-        } 
+        }
+
+        public IActionResult OnGetExportCoursesStudent()
+        {
+            var sessionValue = HttpContext.Session.GetString("Session_User");
+            if (sessionValue != null)
+            {
+                int userId = int.Parse(sessionValue);
+                using (PROJECT_PRUContext _context = new PROJECT_PRUContext())
+                {
+                    var user = _context.Users.Include(t => t.EnroledCourses).FirstOrDefault(t => t.Id == userId);
+
+                    List<Course> courses = new List<Course>();
+                    List<float> coursePoints = new List<float>();
+                    List<EnroledCourse> enroledCourses = user.EnroledCourses.ToList();
+                    if (user.Role == "Student")
+                    {
+                        foreach (EnroledCourse e in user.EnroledCourses)
+                        {
+                            Course course = _context.Courses.Include(t => t.User).FirstOrDefault(x => x.Id == e.CourseId);
+                            if (course != null)
+                            {
+                                courses.Add(course);
+                                coursePoints.Add(getPoint(userId, course.Id));
+                            }
+                        }
+                    }
+                    else
+                    {
+                        courses = _context.Courses.Where(t => t.UserId == userId).ToList();
+                    }
+
+                    using (var workbook = new XLWorkbook())
+                    {
+                        var worksheet = workbook.Worksheets.Add("Courses");
+
+                        worksheet.Cell(1, 1).Value = "Course Title";
+                        worksheet.Cell(1, 2).Value = "Teacher";
+                        worksheet.Cell(1, 3).Value = "Enrole Date";
+                        worksheet.Cell(1, 4).Value = "Your Score";
+
+                        for (int i = 0; i < courses.Count; i++)
+                        {
+                            worksheet.Cell(i + 2, 1).Value = courses[i].Title;
+                            worksheet.Cell(i + 2, 2).Value = courses[i].User.Username;
+                            worksheet.Cell(i + 2, 3).Value = enroledCourses[i].CreatedAt;
+                            worksheet.Cell(i + 2, 4).Value = coursePoints[i] +  "/ 10";
+                        }
+
+                        using (var stream = new MemoryStream())
+                        {
+                            workbook.SaveAs(stream);
+                            var content = stream.ToArray();
+
+                            return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Courses.xlsx");
+                        }
+                    }
+                }
+            }
+
+            return RedirectToPage("Authentication/login");
+        }
+
+        public IActionResult OnGetExportCoursesLecturer()
+        {
+            var sessionValue = HttpContext.Session.GetString("Session_User");
+            if (sessionValue != null)
+            {
+                int userId = int.Parse(sessionValue);
+                using (PROJECT_PRUContext _context = new PROJECT_PRUContext())
+                {
+                    var user = _context.Users.Include(t => t.EnroledCourses).FirstOrDefault(t => t.Id == userId);
+                    List<Course> courses = _context.Courses.Where(t => t.UserId == userId).ToList();
+                    List<int> enroleNums = new List<int>();
+                    foreach (var course in courses)
+                    {
+                        var list = _context.EnroledCourses.Where(t => t.CourseId == course.Id).ToList();
+                        if (list.Any())
+                        {
+                            enroleNums.Add(list.Count);
+                        }
+                        else
+                        {
+                            enroleNums.Add(0);
+                        }
+                    }
+
+
+                    using (var workbook = new XLWorkbook())
+                    {
+                        var worksheet = workbook.Worksheets.Add("Courses");
+
+                        worksheet.Cell(1, 1).Value = "Course Title";
+                        worksheet.Cell(1, 2).Value = "Created Date";
+                        worksheet.Cell(1, 3).Value = "Enrole Nums";
+
+                        for (int i = 0; i < courses.Count; i++)
+                        {
+                            worksheet.Cell(i + 2, 1).Value = courses[i].Title;
+                            worksheet.Cell(i + 2, 2).Value = courses[i].CreatedAt;
+                            worksheet.Cell(i + 2, 3).Value = enroleNums[i];
+                        }
+
+                        using (var stream = new MemoryStream())
+                        {
+                            workbook.SaveAs(stream);
+                            var content = stream.ToArray();
+
+                            return File(content, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "Courses.xlsx");
+                        }
+                    }
+                }
+            }
+
+            return RedirectToPage("Authentication/login");
+        }
     }
 }
